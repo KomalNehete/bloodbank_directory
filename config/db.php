b65cb9c192124e98b639a235f34b57f69861c0e9
KomalNehete <komalnehete28@gmail.com>
2025-12-12 17:31:59 +0530

    add changes

--- config/db.php
+++ config/db.php
@@ -1,13 +1,13 @@
 <?php
-$host = "localhost:3307";  
-$user = "root";       
-$pass = "";           
-$db   = "bloodbank_db";
+$host = "sql113.infinityfree.com	
+";   // your MySQL host from InfinityFree
+$db   = "if0_40664898_bloodbank"; // your database name
+$user = "if0_40664898";       // your database username
+$pass = "Ibf4k2KnDiL";   // your InfinityFree account password
 
 $conn = new mysqli($host, $user, $pass, $db);
 
-// Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Database connection failed: " . $conn->connect_error);
 }
 ?>
